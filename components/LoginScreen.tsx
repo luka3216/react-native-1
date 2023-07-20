e29d599b28e9ee0147be3fb495d9e4293a553acc
luka3216 <lkiri15@freeuni.edu.ge>
2023-07-20 20:53:07 +0400

    Pressable

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, StyleSheet } from "react-native";
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  StyleSheet,
+  FlatList,
+  SectionList,
+  Pressable,
+} from "react-native";
 
 export default function LoginScreen() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  return (
+
+  return isLoggedIn ? (
+    LoggedInView()
+  ) : (
     <ScrollView style={styles.container}>
       <Text style={styles.headerText}>Welcome to Little Lemon</Text>
       <Text style={styles.regularText}>Login to continue</Text>
@@ -23,6 +35,12 @@ export default function LoginScreen() {
         value={password}
         onChangeText={setPassword}
       ></TextInput>
+      <Pressable
+        style={styles.loginButton}
+        onPress={() => setIsLoggedIn(true)}
+      >
+        <Text style={styles.loginText}>Login</Text>
+      </Pressable>
     </ScrollView>
   );
 }
@@ -48,5 +66,45 @@ const styles = StyleSheet.create({
     margin: 12,
     padding: 8,
     backgroundColor: "white",
+  },
+  loginButton: {
+    marginTop: 24,
+    borderRadius: 24,
+    height: 48,
+    backgroundColor: "white",
+    alignSelf: "center",
+    paddingHorizontal: 48,
+    justifyContent: "center",
+  },
+  loginText: {
+    fontSize: 16,
+  },
+});
+
+function LoggedInView() {
+  return (
+    <ScrollView style={loggedInStyles.view}>
+      <Text style={loggedInStyles.title}>Welcome to Little Lemon</Text>
+      <Text style={loggedInStyles.subTitle}>Your are logged in!</Text>
+    </ScrollView>
+  );
+}
+
+const loggedInStyles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignContent: "center",
+  },
+  title: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 32,
+    padding: 24,
+  },
+  subTitle: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 24,
+    padding: 24,
   },
 });
