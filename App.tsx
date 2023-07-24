8c07b94d886ec7fe05497dcb697dd52f76ef63d4
luka3216 <lkiri15@freeuni.edu.ge>
2023-07-24 06:48:07 +0400

    Tab navigation

--- App.tsx
+++ App.tsx
@@ -9,16 +9,42 @@ import LoginScreen from "./components/LoginScreen";
 import WelcomeWithLogo from "./components/WelcomeWithLogo";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { IoniconsIconName } from "./lib/HelperTypes";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Welcome" component={WelcomeWithLogo}></Stack.Screen>
-        <Stack.Screen name="Login" component={LoginScreen}></Stack.Screen>
-      </Stack.Navigator>
+      <Tab.Navigator
+        initialRouteName="Login"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: IoniconsIconName;
+            if (route.name === "Welcome") {
+              if (focused) {
+                iconName = "ios-information-circle";
+              } else {
+                iconName = "ios-information-circle-outline";
+              }
+            } else if (route.name === "Login") {
+              if (focused) {
+                iconName = "ios-log-in";
+              } else {
+                iconName = "ios-log-in-outline";
+              }
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Welcome" component={WelcomeWithLogo}></Tab.Screen>
+        <Tab.Screen name="Login" component={LoginScreen}></Tab.Screen>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- lib/HelperTypes.ts
+++ lib/HelperTypes.ts
@@ -1,3 +1,8 @@
 import type { createNativeStackNavigator } from "@react-navigation/native-stack";
+import type { Icon } from "@expo/vector-icons/build/createIconSet";
+import { Ionicons } from "@expo/vector-icons";
 
 export type ScreenFunctionComponent = ReturnType<Parameters<ReturnType<typeof createNativeStackNavigator>['Screen']>[0]['component'] & React.FunctionComponent>;
+
+type ExtractIcon<T> = T extends Icon<infer U, "ionicons"> ? U : never;
+export type IoniconsIconName = ExtractIcon<typeof Ionicons>;
